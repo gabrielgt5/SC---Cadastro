7d53fe1ebf88553f6a9093ce6750060f2237beeb
Gabriel Lucas <69332675+gabrielgt5@users.noreply.github.com>
2022-12-01 00:15:01 -0400

    Update sistema.php

--- app/views/sistema.php
+++ app/views/sistema.php
@@ -44,15 +44,16 @@
             justify-content: center;
             gap: .1%;
         }
+
+        .bg{
+            background-color: indigo;
+        }
     </style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Sistema de registro</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
         </div>
         <div class="d-flex">
             <a href="../controllers/sair.php" class="btn btn-danger me-5">Sair</a>
@@ -65,7 +66,7 @@
     <br>
     <div class="box-search">
         <input type="search" class="form-control w-25" placeholder="Pesquisar" id="pesquisar">
-        <button onclick="searchData()" class="btn btn-primary">
+        <button onclick="searchData()" class="bg btn btn-primary">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
             </svg>
